c617211f57226a3e932af99f4fc5f6cde0c2cc13
2418 <Edward.Bronshtein@RetailDirections.com>
2020-04-19 17:48:56 +1000

    First commit.

---
+++ ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConfiguringApps.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UptimeService uptime;
+        private ILogger<HomeController> logger;
+
+        public HomeController(UptimeService up, ILogger<HomeController> log)
+        {
+            uptime = up;
+            logger = log;
+        }
+
+        public ViewResult Index(bool throwException = false)
+        {
+            logger.LogDebug($"Handled {Request.Path} at uptime {uptime.Uptime}");
+
+            if (throwException)
+            {
+                throw new System.NullReferenceException();
+            }
+
+            return View(new Dictionary<string, string>
+            {
+                ["Message"] = "This is the Index action",
+                ["Uptime"] = $"{uptime.Uptime}ms"
+            });
+        }
+
+        public ViewResult Error => View(nameof(Index), new Dictionary<string, string> { ["Message"] = "This is the Error action" });
+    }
+}
---
+++ ConfiguringApps/ConfiguringApps/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConfiguringApps.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConfiguringApps
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<UptimeService>();
+            services.AddMvc().AddMvcOptions(options => 
+            {
+                options.RespectBrowserAcceptHeader = true;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}

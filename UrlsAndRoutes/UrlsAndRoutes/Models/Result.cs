ce6891b11c14b6e6b63b50ea2980086a60b00eb5
2418 <Edward.Bronshtein@RetailDirections.com>
2020-04-19 18:07:38 +1000

    HomeController added.

--- UrlsAndRoutes/UrlsAndRoutes/Models/Result.cs
+++ UrlsAndRoutes/UrlsAndRoutes/Models/Result.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace UrlsAndRoutes.Models
 {
     public class Result
     {
+        public string Controller { get; set; }
+
+        public string Action { get; set; }
+
+        public IDictionary<string, object> Data { get; } = new Dictionary<string, object>();
     }
 }

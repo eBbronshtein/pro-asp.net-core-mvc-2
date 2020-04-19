7d09e3041815a662d4a92b29feda8fc741edbb66
2418 <Edward.Bronshtein@RetailDirections.com>
2020-04-19 18:20:05 +1000

    View added.

--- UrlsAndRoutes/UrlsAndRoutes/Controllers/AdminController.cs
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/AdminController.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Models;
 
 namespace UrlsAndRoutes.Controllers
 {
-    public class AdminController
+    public class AdminController : Controller
     {
+        public ViewResult Index() => View
+        (
+            "Result",
+            new Result
+            {
+                Controller = nameof(AdminController),
+                Action = nameof(Index)
+            }
+        );
     }
 }

45e6b857a5293013c5d42c69883c59d9580154a2
2418 <Edward.Bronshtein@RetailDirections.com>
2020-04-20 18:48:28 +1000

    Constraining a Route Using a Regular Expression

--- UrlsAndRoutes/UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/UrlsAndRoutes/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace UrlsAndRoutes
@@ -24,28 +25,22 @@ namespace UrlsAndRoutes
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "MyRoute",
-                    template: "{controller=Home}/{action=Index}/{id?}/{*catchall}");
+                    name: "MyRoute1",
+                    template: "{controller:regex(^H.*)=Home}/{action=Index}/{id?}");
 
-                //routes.MapRoute(
-                //    name: "ShopSchema2",
-                //    template: "Shop/OldAction",
-                //    defaults: new { controller = "Home", action = "Index" });
+                routes.MapRoute(
+                    name: "MyRoute2",
+                    template: "{controller=Home}/{action=Index}/{id:int?}");
 
-                //routes.MapRoute(
-                //    name: "ShopSchema",
-                //    template: "Shop/{action}",
-                //    defaults: new { controller = "Home" });
+                routes.MapRoute(
+                    name: "MyRoute3",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" },
+                    constraints: new { id = new IntRouteConstraint() });
 
-                //routes.MapRoute("", "X{controller}/{action}");
-
-                //routes.MapRoute(
-                //    name: "default",
-                //    template: "{controller=Home}/{action=Index}");
-
-                //routes.MapRoute(
-                //    name: "MyRoute", 
-                //    template: "Public/{controller=Home}/{action=Index}");
+                routes.MapRoute(
+                    name: "MyRoute4",
+                    template: "{controller:regex(^H.*)=Home}/" + "{action:regex(^Index$|^About$)=Index}/{id?}");
             });
         }
     }

884799258bac222384bc216f26e0fbffc028dcac
2418 <Edward.Bronshtein@RetailDirections.com>
2020-04-22 18:39:53 +1000

    Creating a More Complex Route

--- UrlsAndRoutes/UrlsAndRoutes/Controllers/CustomerController.cs
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/CustomerController.cs
@@ -2,9 +2,9 @@
 using UrlsAndRoutes.Models;
 namespace UrlsAndRoutes.Controllers
 {
+    [Route("app/[controller]/actions/[action]/{id?}")]
     public class CustomerController : Controller
     {
-        [Route("[controller]/MyAction")]
         public ViewResult Index() => View(
             "Result",
             new Result

818a6d9108e7765927bc10330c27d8e1c4544464
2418 <Edward.Bronshtein@RetailDirections.com>
2020-04-27 08:20:05 +1000

    Generating a URL from a Specific Route

--- UrlsAndRoutes/UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/UrlsAndRoutes/Startup.cs
@@ -33,6 +33,10 @@ namespace UrlsAndRoutes
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "out",
+                    template: "outbound/{controller=Home}/{action=Index}");
             });
         }
     }

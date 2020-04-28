c073ff86170aabd4dbde18680476ca52d93be45b
Edward Bronshtein <BronshteinE@retaildirections.com>
2020-04-28 18:50:58 +1000

    Using the MVC Controller API

--- ControllersAndActions/ControllersAndActions/Controllers/PocoController.cs
+++ ControllersAndActions/ControllersAndActions/Controllers/PocoController.cs
@@ -1,7 +1,18 @@
-﻿namespace ControllersAndActions.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace ControllersAndActions.Controllers
 {
     public class PocoController
     {
-        public string Index() => "This is a POCO controller";
+        public ViewResult Index() => new ViewResult()
+        {
+            ViewName = "Result",
+            ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary())
+            {
+                Model = $"This is a POCO controller"
+            }
+        };
     }
 }

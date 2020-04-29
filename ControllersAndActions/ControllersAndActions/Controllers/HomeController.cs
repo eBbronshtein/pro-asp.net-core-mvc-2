159c776456f0154a177038531e6b3f219982974d
2418 <Edward.Bronshtein@RetailDirections.com>
2020-04-29 18:26:00 +1000

    Producing a Response Using the Context Object

--- ControllersAndActions/ControllersAndActions/Controllers/HomeController.cs
+++ ControllersAndActions/ControllersAndActions/Controllers/HomeController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
 namespace ControllersAndActions.Controllers
 {
     public class HomeController : Controller
     {
         public ViewResult Index() => View("SimpleForm");
 
-        public ViewResult ReceiveForm(string name, string city) => View("Result", $"{name} lives in {city}");
+        public void ReceiveForm(string name, string city)
+        {
+            Response.StatusCode = 200;
+            Response.ContentType = "text/html";
+            byte[] content = Encoding.ASCII.GetBytes($"<html><body>{name} lives in {city}</body>");
+            Response.Body.WriteAsync(content, 0, content.Length);
+        }
     }
 }

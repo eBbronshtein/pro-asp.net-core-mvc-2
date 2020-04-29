bb6a079ce5aece278f8209327d592b200c7171cc
2418 <Edward.Bronshtein@RetailDirections.com>
2020-04-29 18:50:53 +1000

    Understanding the Search for a View File

---
+++ ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("/Views/Admin/Index");
+        }
+    }
+}

0c9aa5a8eedc6fdd40c8254675915afd7841c6ea
2418 <Edward.Bronshtein@RetailDirections.com>
2020-05-05 18:56:55 +1000

    Sending a Specific HTTP Result Code

--- ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
+++ ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 
 namespace ControllersAndActions.Controllers
 {
     public class ExampleController : Controller
     {
-        public VirtualFileResult Index() => File(@"~/twitter-bootstrap/css/bootstrap.min.css", "text/css");
+        public StatusCodeResult Index() => StatusCode(StatusCodes.Status404NotFound);
     }
 }

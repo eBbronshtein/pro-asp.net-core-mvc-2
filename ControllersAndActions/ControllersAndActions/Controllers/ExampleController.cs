4b158766188fe5dfe2cbcadc667ed6a6808ba3c1
2418 <Edward.Bronshtein@RetailDirections.com>
2020-05-05 18:50:21 +1000

    Responding with the Contents of Files

--- ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
+++ ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
@@ -5,8 +5,6 @@ namespace ControllersAndActions.Controllers
 {
     public class ExampleController : Controller
     {
-        //public ContentResult Index() => Content("[\"Alice\",\"Bob\",\"Joe\"]", "application/json");
-
-        public ObjectResult Index() => Ok(new string[] { "Alice", "Bob", "Joe" });
+        public VirtualFileResult Index() => File(@"~/twitter-bootstrap/css/bootstrap.min.css", "text/css");
     }
 }

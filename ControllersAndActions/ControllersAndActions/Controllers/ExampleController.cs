042dbc6874f86ac34dcdb156dc67bd4b51b367e2
2418 <Edward.Bronshtein@RetailDirections.com>
2020-05-05 18:28:39 +1000

    Producing a JSON Response

--- ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
+++ ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
@@ -1,23 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 
-// CH 3
-
 namespace ControllersAndActions.Controllers
 {
     public class ExampleController : Controller
     {
-        public ViewResult Index()
-        {
-            ViewBag.Message = "Hello";
-            ViewBag.Date = DateTime.Now;
-            return View();
-        }
-
-        public ViewResult Result() => View((object)"Hello World");
-
-        public RedirectToActionResult Redirect() => RedirectToAction(nameof(Index));
-
-        // public RedirectToActionResult Redirect() => RedirectToAction(nameof(HomeController), nameof(HomeController.Index));
+        public JsonResult Index() => Json(new[] { "Alice", "Bob", "Joe" });
     }
 }

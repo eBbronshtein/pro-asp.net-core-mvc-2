b3a4f585b0356b6bf81de3be1f81ac728681310d
2418 <Edward.Bronshtein@RetailDirections.com>
2020-05-05 18:22:56 +1000

    Using Temp Data

--- ControllersAndActions/ControllersAndActions/Controllers/HomeController.cs
+++ ControllersAndActions/ControllersAndActions/Controllers/HomeController.cs
@@ -9,8 +9,20 @@ namespace ControllersAndActions.Controllers
         public ViewResult Index() => View("SimpleForm");
 
         [HttpPost]
-        public RedirectToActionResult ReceiveForm(string name, string city) => RedirectToAction(nameof(Data));
+        public RedirectToActionResult ReceiveForm(string name, string city)
+        {
+            TempData["name"] = name;
+            TempData["city"] = city;
 
-        public ViewResult Data() => View("Result");
+            return RedirectToAction(nameof(Data));
+        }
+
+        public ViewResult Data()
+        {
+            string name = TempData["name"] as string;
+            string city = TempData["city"] as string;
+
+            return View("Result", $"{name} lives in {city}");
+        }
     }
 }
